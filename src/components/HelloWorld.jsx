cadfca5126b7036b66ec9fa502eee040c290a53c
chato1337 <chato1337@gmail.com>
2020-07-31 11:13:28 -0500

    enviorment setup

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../assets/styles/App.scss'
+
+const HelloWorld = () => {
+    return(
+        <div>
+            <h1>holi</h1>
+            <p>chatuz park</p>
+        </div>
+    )
+}
+
+export default HelloWorld
